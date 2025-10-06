0c27eff4292a50f02220a5342ac487908a0df029
angynuevo2 <angygmz366@gmail.com>
2025-10-05 20:00:41 -0600

    cambiosssss

--- app/(main)/seguridad/usuario/page.tsx
+++ app/(main)/seguridad/usuario/page.tsx
@@ -1,6 +1,5 @@
-export const dynamic = 'force-dynamic';
 'use client';
-
+export const dynamic = 'force-dynamic';
 
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
